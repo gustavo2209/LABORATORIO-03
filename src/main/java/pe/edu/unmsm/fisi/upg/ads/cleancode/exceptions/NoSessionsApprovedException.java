007f83e447bdf4bfcf843bee93adf75ed8480e66
Gustavo Chimoy García <gchimoy@drelm.gob.pe>
2018-09-26 12:12:17 -0500

    Update NoSessionsApprovedException.java

--- src/main/java/pe/edu/unmsm/fisi/upg/ads/cleancode/exceptions/NoSessionsApprovedException.java
+++ src/main/java/pe/edu/unmsm/fisi/upg/ads/cleancode/exceptions/NoSessionsApprovedException.java
@@ -1,1 +1,15 @@
+package pe.edu.unmsm.fisi.upg.ads.cleancode.exceptions;
 
+import java.io.Serializable;
+
+public class NoSessionsApprovedException extends Exception implements Serializable {
+  
+    private static final long serialVersionUID = 1L;
+    
+    public NoSessionsApprovedException(String message) {
+      
+        super(message);
+    
+    }
+  
+}
