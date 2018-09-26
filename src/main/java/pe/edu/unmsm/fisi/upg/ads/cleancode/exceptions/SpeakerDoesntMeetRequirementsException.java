5f921cab69abe9525a198646db7013fdefd9ef58
Gustavo Chimoy García <gchimoy@drelm.gob.pe>
2018-09-26 12:13:51 -0500

    Update SpeakerDoesntMeetRequirementsException.java

--- src/main/java/pe/edu/unmsm/fisi/upg/ads/cleancode/exceptions/SpeakerDoesntMeetRequirementsException.java
+++ src/main/java/pe/edu/unmsm/fisi/upg/ads/cleancode/exceptions/SpeakerDoesntMeetRequirementsException.java
@@ -1,1 +1,21 @@
+package pe.edu.unmsm.fisi.upg.ads.cleancode.exceptions;
 
+import java.io.Serializable;
+
+public class SpeakerDoesntMeetRequirementsException extends Exception implements Serializable {
+  
+    private static final long serialVersionUID = 1L;
+  
+    public SpeakerDoesntMeetRequirementsException(String message) {
+      
+        super(message);
+    
+    }
+  
+    public SpeakerDoesntMeetRequirementsException(String format, Object[] args) {
+      
+        super(String.format(format, args));
+    
+    }
+  
+}
